836270a82f53c9477c8bded613bd1c8f7f28fb18
Nikita <90nk3666@gmail.com>
2024-07-07 18:43:28 +0300

    DZ Python

---
+++ main.py
@@ -0,0 +1,15 @@
+# task_1 'Arifmetika'
+# 1st program
+print(9 ** 0.5 * 5)
+
+# task_2 'Sravnenie, or, and'
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# task_3 'Slozhnaja arifmetika'
+# 3rd program
+print(1234 % 1000 // 10 + 5678 % 5000 // 10)
+
+# task_4 'Vse, vezde i srazy'
+# 4th program
+print(int(13.42 * 100) % 100 == int(42.13))
