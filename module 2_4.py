512a4e43930b35cb82015fe925d1bd6c4d245110
Nikita Kulikov <100667111+Koolkv@users.noreply.github.com>
2024-08-12 10:32:27 +0300

    Update module 2_4.py
    Доработка ДЗ с циклами

--- module 2_4.py
+++ module 2_4.py
@@ -1,22 +1,45 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
-is_prime = True # trebuetsja raz'jasnenija na temu flagov, dlja menja tema ne raskrita
+
+
 for i in numbers:
-    num = 0
-    if i == 1: # 1 - ne prostoe i ne sostavnoe chislo
-        print('it is number one, not bad')
-    else:
-        for k in range(2, i + 1):
-            if i == 2: # 2 - prostoe chislo
-                primes.append(i)
-            if i != 2 and i % k == 0:
-                num += 1
-            if num >= 2:
-                not_primes.append(i)
-                break
-    if num == 1:
+    is_prime = True
+    if i == 1:
+        continue
+    for k in range(2, i):
+        if i % k == 0:
+            is_prime = False
+            break
+    if is_prime:
         primes.append(i)
+    else:
+        not_primes.append(i)
+
 
 print(primes)
 print(not_primes)
+
+#------------    Ниже первое решение задачи - не зачет    ------------
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+# primes = []
+# not_primes = []
+# is_prime = True # trebuetsja raz'jasnenija na temu flagov, dlja menja tema ne raskrita
+# for i in numbers:
+#     num = 0
+#     if i == 1: # 1 - ne prostoe i ne sostavnoe chislo
+#         print('it is number one, not bad')
+#     else:
+#         for k in range(2, i + 1):
+#             if i == 2: # 2 - prostoe chislo
+#                 primes.append(i)
+#             if i != 2 and i % k == 0:
+#                 num += 1
+#             if num >= 2:
+#                 not_primes.append(i)
+#                 break
+#     if num == 1:
+#         primes.append(i)
+
+# print(primes)
+# print(not_primes)
