e1dace5ce34009a34aaf6223971fe0070c52813a
Nikita Kulikov <100667111+Koolkv@users.noreply.github.com>
2024-08-15 13:09:18 +0300

    module_3_hard
    Это была потная работа. Дня два наверное думал, экспериментировал. Наверное опытный программист решит задачку за 15 сек.

---
+++ module_3_hard.py
@@ -0,0 +1,26 @@
+data_structure = [[1, 2, 3], {'a': 4, 'b': 5}, (6, {'cube': 7, 'drum': 8}), "Hello",
+                  ((), [{(2, 'Urban', ('Urban2', 35))}])]
+
+
+def unpacks(data):
+    for item in data:
+        if isinstance(item, list):
+            for sub_item in unpacks(item):
+                yield sub_item
+        elif isinstance(item, dict):
+            for sub_item in unpacks(item.items()):
+                yield sub_item
+        elif isinstance(item, tuple):
+            for sub_item in unpacks(item):
+                yield sub_item
+        elif isinstance(item, set):
+            for sub_item in unpacks(item):
+                yield sub_item
+        elif isinstance(item, str):
+            yield len(item)
+
+        else:
+            yield item
+
+
+print(sum(list(unpacks(data_structure))))
