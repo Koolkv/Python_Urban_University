4ac96cbec35f047914c36872afc2c40ca8e6af09
Nikita Kulikov <100667111+Koolkv@users.noreply.github.com>
2024-08-16 11:02:38 +0300

    module_4_1
    Fake_math
    True_math
    Functions and import modules

---
+++ fake_math.py
@@ -0,0 +1,5 @@
+def divide(first, second):
+    if second == 0:
+        return print('Ошибка! Здесь делить на ноль низя)')
+    else:
+        return print(first / second)
---
+++ module_4_1.py
@@ -0,0 +1,7 @@
+import fake_math as fm
+import true_math as tm
+
+fm.divide(10, 5)
+fm.divide(9, 0)
+tm.divide(6, 2)
+tm.divide(4, 0)
